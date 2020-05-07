b4a0c2a9c8ea7cc7956d70db7c965ef98a9d4462
Kuba Werłos <werlos@gmail.com>
2020-05-07 18:10:46 +0200

    Update VersionSelector usage to new API

---
+++ src/Adapter/AdapterInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of composer-require-better.
+ *
+ * (c) 2020 Kuba Werłos
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace RequireBetter\Adapter;
+
+use Composer\Package\PackageInterface;
+use Composer\Package\Version\VersionSelector;
+use Composer\Repository\PlatformRepository;
+
+/**
+ * @internal
+ */
+interface AdapterInterface
+{
+    /**
+     * @return bool|PackageInterface
+     */
+    public function findBestCandidate(VersionSelector $versionSelector, string $package, string $targetPhpVersion);
+
+    /**
+     * Composer 1 needs Composer\DependencyResolver\Pool
+     * Composer 2 needs Composer\Repository\RepositorySet
+     *
+     * @return \Composer\DependencyResolver\Pool|\Composer\Repository\RepositorySet
+     */
+    public function getRepositorySet();
+
+    /**
+     * @param \Composer\DependencyResolver\Pool|\Composer\Repository\RepositorySet $repositorySet
+     */
+    public function createVersionSelector($repositorySet, PlatformRepository $platformRepo): VersionSelector;
+}
---
+++ src/Adapter/Composer2Adapter.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of composer-require-better.
+ *
+ * (c) 2020 Kuba Werłos
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace RequireBetter\Adapter;
+
+use Composer\Package\Version\VersionSelector;
+use Composer\Repository\PlatformRepository;
+use Composer\Repository\RepositorySet;
+
+/**
+ * @internal
+ */
+final class Composer2Adapter implements AdapterInterface
+{
+    public function findBestCandidate(VersionSelector $versionSelector, string $package, string $targetPhpVersion)
+    {
+        return $versionSelector->findBestCandidate($package);
+    }
+
+    public function getRepositorySet()
+    {
+        return new RepositorySet();
+    }
+
+    public function createVersionSelector($repositorySet, PlatformRepository $platformRepo): VersionSelector
+    {
+        return new VersionSelector($repositorySet, $platformRepo);
+    }
+}
