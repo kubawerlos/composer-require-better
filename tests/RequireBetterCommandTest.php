afb791d8b2d0e3f5ac01a61d1ee0165b65714904
Kuba Werłos <werlos@gmail.com>
2020-10-15 21:43:34 +0200

    Fix: make tests ignore warnings

--- tests/RequireBetterCommandTest.php
+++ tests/RequireBetterCommandTest.php
@@ -222,9 +222,11 @@ final class RequireBetterCommandTest extends TestCase
 
         $runner = new Runner();
         $requireOutput = $runner->run('require', ['packages' => $packages, '--no-update' => true]);
+        $requireOutput = \trim(\preg_replace('/<warning>.*<\/warning>/', '', $requireOutput));
 
         $runner = new Runner();
         $requireBetterOutput = $runner->run('rb', ['packages' => $packages, '--no-update' => true]);
+        $requireBetterOutput = \trim(\preg_replace('/<warning>.*<\/warning>/', '', $requireBetterOutput));
 
         self::assertSame(4, \levenshtein($requireOutput, $requireBetterOutput));
     }
