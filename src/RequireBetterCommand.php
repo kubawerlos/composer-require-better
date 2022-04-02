8ca7e5705f4ebc2a1e281ece6b76c2aefc973423
Kuba Werłos <werlos@gmail.com>
2022-04-02 10:03:56 +0200

    CS fix

--- src/RequireBetterCommand.php
+++ src/RequireBetterCommand.php
@@ -12,7 +12,6 @@
 namespace RequireBetter;
 
 use Composer\Command\RequireCommand;
-use Composer\Composer;
 use Composer\Package\PackageInterface;
 use Composer\Package\Version\VersionSelector;
 use Composer\Repository\CompositeRepository;
